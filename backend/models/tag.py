05244acf0f5619b7b651bba826d52e49a18b214c
Devang Jalag <zj799583@dal.ca>
2025-10-28 23:19:20 -0300

    fix: minor changes

--- backend/models/tag.py
+++ backend/models/tag.py
@@ -1,6 +1,6 @@
 """
 Description: Abstract base model for all database tables using SQLAlchemy.
-Author: Devang Patel
+Author: Devang Jalag
 Created: 2025-10-25
 Last Modified: 
     2025-10-26 - File created and implemented basic CRUD operations.
