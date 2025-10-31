ecd7c0865bb2af63cfe283744040f0ce6009b8b8
y-onee <saayonee05@gmail.com>
2025-10-31 04:07:09 -0300

    fix: config file fix

--- backend/config/config_postgres.py
+++ backend/config/config_postgres.py
@@ -4,7 +4,7 @@ class Config:
     # Make sure this matches your DataGrip connection exactly
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "postgresql://myuser:dal1234@localhost/dalgroup2db"
+        "postgresql://myuser:dal1234@localhost:5432/dalgroup2db"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
