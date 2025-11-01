2c906d27295e296f00f82c0ee16c63ae603becde
Mahek Shah <maheks1713@gmail.com>
2025-11-01 18:36:42 -0300

    feat: config changes

--- backend/config/config_postgres.py
+++ backend/config/config_postgres.py
@@ -1,10 +1,19 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+port = os.getenv("PORT")
+
+database_url = os.getenv("DB_URL") 
+
+print(f"hello {port}")
 
 class Config:
     # Make sure this matches your DataGrip connection exactly
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL",
-        "postgresql://myuser:dal1234@localhost:5432/dalgroup2db"
+        database_url
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
--- backend/config/config_supabase.py
+++ backend/config/config_supabase.py
@@ -1,6 +1,14 @@
-from supabase import create_client
+import os
+from supabase import create_client, Client
+url="https://pqygcttjdvxynizbwwui.supabase.co"
+key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxeWdjdHRqZHZ4eW5pemJ3d3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2MzAwODIsImV4cCI6MjA3NzIwNjA4Mn0.n7GqoL9mHeDe_Po6fT6Mz77Oj9X64WDEFOyf7emwsgM"
+supabase: Client = create_client(url, key)
 
-url = "postgresql://postgres:Dal@123@db.pqygcttjdvxynizbwwui.supabase.co:5432/postgres"
-key = "16480"
-supabase = create_client(url, key)
+
+response = supabase.table('User').select('*').limit(1).execute()
+
+if response.data is not None:
+    print('Connection successful:', response.data)
+else:
+    print('Connection failed. Error:', response.error)
 
