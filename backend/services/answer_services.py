1e24b29e46fbf18c56477a5690bf5da470166e04
Saayonee Dhepe <lf399654@dal.ca>
2025-11-23 23:16:03 -0400

    test: minimal code

--- backend/services/answer_services.py
+++ backend/services/answer_services.py
@@ -7,7 +7,9 @@ Last Modified By: Saayonee Dhepe
 Last Modified: 2025-11-23
 """
 
-
+from models.question import Question
+from models.answer import Answer
+from datetime import datetime
 class AnswerServices:
     def __init__(self):
         pass
@@ -20,4 +22,11 @@ class AnswerServices:
         if len(stripped_body) < 20:
             return False
         
+        return True
+    
+    def question_exists(self, question_id):
+        question = Question.query.get(question_id)
+        return question is not None
+    
+    def create_answer(self, question_id, user_id, body):
         return True
